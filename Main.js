d8c608733f497a6d202eae1300a544a098d5c121
Nada Marei <68446499+Nadamarey@users.noreply.github.com>
2022-05-08 15:32:21 +0200

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,45 @@
+var mysong = document.getElementById("mysong");
+var icon = document.getElementById("icon");
+
+icon.onclick = function(){
+    if(mysong.paused){
+        mysong.play();
+        icon.src = "pause.png";
+    }else{
+        mysong.pause();
+        icon.src = "play.png";
+    }
+}
+
+    let stars =document.getElementById('stars');
+    let stars2 =document.getElementById('stars2');
+    let moon =document.getElementById('moon');
+    let rock =document.getElementById('rock');
+    let masjid =document.getElementById('masjid');
+    let text =document.getElementById('text');
+    let header =document.getElementById('header');
+
+    window.addEventListener('scroll' , function(){
+        let value = window.scrollY;
+        stars.style.bottom = value*0.05 + '%';
+        stars2.style.bottom = value*0.025 + '%';
+        moon.style.bottom = value*-0.5 + 'px';
+        rock.style.bottom = value*-0.75 + 'px';
+        masjid.style.bottom = value*-0.15 + 'px';
+
+
+        text.style.right = -100 + value * 0.5 + '%';
+        text.style.top = 50 + value * 0.1 + '%';
+
+        header.style.top = value * 0.5 + 'px';
+    })
+
+
+    const toggleMenu = document.querySelector('.toggle');
+    const navigation = document.querySelector('.navigation');
+    toggleMenu.onclick = function(){
+        toggleMenu.classList.toggle('active');
+        navigation.classList.toggle('active');
+    }
+
+
